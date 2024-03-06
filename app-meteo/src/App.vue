66ec5ab47422a9ef5ad28ce517d99940245b6854
KC Sélim <145485788+SelimXV@users.noreply.github.com>
2024-03-06 15:35:07 +0000

    Ajout du Projet Meteo sur GitHub

---
+++ app-meteo/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import SearchInput from "@/components/SearchInput.vue";
+</script>
+
+<template>
+  <main>
+    <!-- Date -->
+    <div class="text-center mb-6">
+      {{
+        capitalizeDate(new Date().toLocaleDateString('fr-FR', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        }))
+      }}
+    </div>
+
+    <!-- Search -->
+    <div>
+      <SearchInput />
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  methods: {
+    capitalizeDate(date) {
+      return date.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    }
+  }
+}
+</script>
